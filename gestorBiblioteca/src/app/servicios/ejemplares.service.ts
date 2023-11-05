d5fc4815334b88515ef9f10a97369604d9a699c8
victorsigma <victorossielgaray1@gmail.com>
2023-11-04 21:47:09 -0600

    Buscador

--- gestorBiblioteca/src/app/servicios/ejemplares.service.ts
+++ gestorBiblioteca/src/app/servicios/ejemplares.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,8 @@ export class EjemplaresService {
 
   constructor(private http:HttpClient) { }
 
-  getEjemplares(ISBN: number) {
-    return this.http.get("http://localhost:3000/api/ejemplares/${ISBN}");
+  getEjemplares(ISBN: number): Observable<any> {
+    return this.http.get(`http://localhost:3000/api/ejemplares/${ISBN}`);
   }
 
 }
